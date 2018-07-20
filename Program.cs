ca866905a652c250973d3b14f126f31a668a3454
gabrielengel1 <gabriel_engel11@web.de>
2018-07-20 15:03:24 +0200

    add csproj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_Parser_Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
